7996965554bd23f900b7718e7a46198d74e542df
superpil <superpil@trgtech.co.kr>
2020-06-05 16:00:58 +0900

    스타일적용완료

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const categoryList = [
   "중식",
   "양식",
   "일식",
-  "편의점식"
+  "편의점식",
+  "전투식량"
 ]
 
 const menuList = {
@@ -33,52 +34,62 @@ const menuList = {
     "규동"
   ],
   "편의점식": [
-    "김밥&라면",
-    "라면&샌드위치"
+    "김밥&&라면",
+    "라면&&샌드위치"
+  ],
+  "전투식량": [
+    "뽀글이",
+    "닭강정&&컵라면"
   ]
 }
 
+const warp = document.querySelector("#warp");
+const shutdownContent = document.querySelector("#shutdown_content");
 const title = document.querySelector("#title");
-const startBtn = document.querySelector("#start_btn");
-const replayBtn = document.querySelector("#replay_btn");
-const categoryBtn = document.querySelector("#category_btn");
+const startBtn = document.querySelector(".start_btn");
+const replayBtn = document.querySelector(".replay_btn");
+const categoryBtn = document.querySelector(".category_btn");
+const userSetBtn = document.querySelector(".user_set_btn");
 const menuBtn = document.querySelector("#menu_btn");
-const categroyNotice = document.querySelector("#category_notice");
-const menuNotice = document.querySelector("#menu_notice");
 const categoryTotal = document.querySelector("#category_total");
 const menuTotal = document.querySelector("#menu_total");
-const categoryContent = document.querySelector("#category_content");
-const menuContent = document.querySelector("#menu_content");
+const categoryContent = document.querySelector(".category_content");
+const menuContent = document.querySelector(".menu_content");
+const menuNotice = document.querySelector("#menu_notice");
+const categroyNotice = document.querySelector("#category_notice");
 let rendomCategory;
 let count = 3;
 
-//shutdown
-const shutdown = () => {
-  title.innerHTML = "지금 당신 마음속에 있는 메뉴를 드세요."
-  startBtn.style.display = "none";
-  categoryContent.style.display = "none";
-  menuContent.style.display = "none";
-}
-
-//replay btn
-replayBtn.addEventListener("click", () => {
-  categoryContent.style.display = "none";
-  menuContent.style.display = "none";
-  categroyNotice.innerHTML = "";
-  menuNotice.innerHTML = "";
-  startBtn.style.display = "block";
-  replayBtn.style.display = "none";
-  categoryBtn.style.display = "block";
-  menuBtn.style.display = "block";
-  rendomCategory;
+//start btn
+startBtn.addEventListener("click", () => {
+  categoryContent.classList.add("show_btn");
+  startBtn.classList.add("hide_btn");
+  userSetBtn.classList.add("hide_btn");
+  displayCategory();
+  count--;
+  if(count === 0){
+    alert("언제까지 메뉴선택만 할래?!");
+    shutdown();
+  }
 })
 
 //category display
 const displayCategory = () => {
   categoryList.forEach( (value, index, array) => {
-    categoryTotal.innerHTML = `카테고리 리스트 : ${array}`;
+    categoryTotal.innerHTML = `< ${array} >`;
   })
 };
+
+//category btn
+categoryBtn.addEventListener("click", () => {
+  menuContent.classList.add("show_btn");
+  categoryBtn.classList.add("hide_btn");
+  const rendomNumbering = rendomNumber(categoryList);
+  const categorySelect = categoryList[rendomNumbering];
+  categroyNotice.innerHTML = categorySelect;
+  rendomCategory = categorySelect;
+  displayMenu(rendomCategory);
+})
 
 //menu display
 const displayMenu = (target) => {
@@ -89,16 +100,35 @@ const displayMenu = (target) => {
   console.log("targetList>>", menuSelect);
 };
 
-//start btn
-startBtn.addEventListener("click", () => {
-  categoryContent.style.display = "block";
-  startBtn.style.display = "none";
-  displayCategory();
-  count--;
-  if(count === 0){
-    alert("언제까지 메뉴선택만 할래?!");
-    shutdown();
+//menu btn
+menuBtn.addEventListener("click", () => {
+  // replayBtn.classList.add("show_content");
+  if(rendomCategory !== undefined && rendomCategory !== null){
+    if(menuList[rendomCategory]){
+      const rendomNumbering = rendomNumber(menuList[rendomCategory]);
+      console.log("menuRendomNumbering>>", rendomNumbering);
+      const menuSelect = menuList[rendomCategory][rendomNumbering];
+      console.log("menuSelect", menuSelect);
+      alert(`축하합니다! 오늘한끼 메뉴는 "${menuSelect}" 입니다!`);
+      menuNotice.innerHTML = menuSelect;
+      menuBtn.classList.add("hide_btn");
+      replayBtn.classList.add("show_btn");
+    }
   }
+})
+
+//replay btn
+replayBtn.addEventListener("click", () => {
+  startBtn.classList.remove("hide_btn");
+  userSetBtn.classList.remove("hide_btn");
+  replayBtn.classList.remove("show_btn");
+  categoryContent.classList.remove("show_btn");
+  menuContent.classList.remove("show_btn");
+  menuBtn.classList.remove("hide_btn");
+  categoryBtn.classList.remove("hide_btn");
+  categroyNotice.innerHTML = "";
+  menuNotice.innerHTML = "";
+  rendomCategory;
 })
 
 //rendom number
@@ -108,28 +138,8 @@ const rendomNumber = (target) => {
   return number;
 }
 
-//category select
-categoryBtn.addEventListener("click", () => {
-  const rendomNumbering = rendomNumber(categoryList);
-  const categorySelect = categoryList[rendomNumbering];
-  categroyNotice.innerHTML = categorySelect;
-  rendomCategory = categorySelect;
-  menuContent.style.display = "block";
-  categoryBtn.style.display = "none";
-  displayMenu(rendomCategory);
-})
-
-//menu select
-menuBtn.addEventListener("click", () => {
-  if(rendomCategory !== undefined && rendomCategory !== null){
-    if(menuList[rendomCategory]){
-      const rendomNumbering = rendomNumber(menuList[rendomCategory]);
-      console.log("menuRendomNumbering>>", rendomNumbering);
-      const menuSelect = menuList[rendomCategory][rendomNumbering];
-      console.log("menuSelect", menuSelect);
-      menuNotice.innerHTML = menuSelect;
-      menuBtn.style.display = "none";
-      replayBtn.style.display = "block";
-    }
-  }
-})
+//shutdown
+const shutdown = () => {
+  warp.style.display = "none";
+  shutdownContent.style.display = "block";
+}
